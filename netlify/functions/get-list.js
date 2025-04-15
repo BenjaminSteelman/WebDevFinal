2a9ffd62c6f2bae2cf739192ce5e714cd1095dcd
BenjaminSteelman <167154828+BenjaminSteelman@users.noreply.github.com>
2025-04-15 14:21:19 -0400

    Update get-list.js

--- netlify/functions/get-list.js
+++ netlify/functions/get-list.js
@@ -18,7 +18,7 @@ exports.handler = async function(event, context) {
 
     return {
         statusCode: 200,
-        body: JSON.stringify(data);
+        body: JSON.stringify(data)
     }
   } catch (error) {
     console.error(error.message);
