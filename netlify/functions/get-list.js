1fa85118c3740e7da52c2c7f2facd6b9379aa5f8
BenjaminSteelman <167154828+BenjaminSteelman@users.noreply.github.com>
2025-04-15 14:19:54 -0400

    Update get-list.js

--- netlify/functions/get-list.js
+++ netlify/functions/get-list.js
@@ -8,12 +8,19 @@ const supabase = createClient(process.env.DATABASE_URL, process.env.SUPABASE_SER
 exports.handler = async function(event, context) {
   try {
     const { data, error } = await supabase
-      .from('CourseList');
-      .select('id, courseList');
+      .from('CourseList')
+      .select('id', 'courseList')
       .order('id', { ascending: false});
 
     if (error) {
       console.error(error.message);
     };
-  };
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(data);
+    }
+  } catch (error) {
+    console.error(error.message);
+  }
 };
