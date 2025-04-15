feb33cdc238115357d7840896c1587f64655724f
BenjaminSteelman <167154828+BenjaminSteelman@users.noreply.github.com>
2025-04-15 14:05:56 -0400

    Update get-list.js

--- netlify/functions/get-list.js
+++ netlify/functions/get-list.js
@@ -1,5 +1,5 @@
-var DATABASE_URL =
-var SUPABASE_SERVICE_API_KEY = 
+var DATABASE_URL = 'https://xcoelbuelwoyrirwptud.supabase.co';
+var SUPABASE_SERVICE_API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhjb2VsYnVlbHdveXJpcndwdHVkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQzMDU5MzcsImV4cCI6MjA1OTg4MTkzN30.RGlbvdU4jcchXV-gJQtEuhMuNP6tVqpET7nwqm5DO4A';
 
 require('dotenv').config();
 const {
